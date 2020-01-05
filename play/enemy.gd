c27122fbb2e1d3af5231a0340d579f0429e2ba7a
AronShrestha <52240049+AronShrestha@users.noreply.github.com>
2020-01-05 21:22:48 +0545

    Upgrade x-y plane

--- play/enemy.gd
+++ play/enemy.gd
@@ -1,24 +1,33 @@
 extends KinematicBody2D
 const GRAVITY =10
-const SPEED=30
+
 const FLOOR=Vector2(0,-1)
 var velocity= Vector2()
 var direction=1
 var is_dead  = false
+export(int) var speed=30
+export(int) var hp=1
+export(Vector2)  var size =Vector2(1,1)
+
 func _ready():
+	scale =size 
 	pass
 	
 func dead():
-	
-	is_dead = true
-	velocity =Vector2(0, 0)
-	$AnimatedSprite.play("dead")
-	$CollisionShape2D.disabled=true
-	$Timer.start()
+	hp -=1
+	if hp <=0:
+		is_dead = true
+		velocity =Vector2(0, 0)
+		$AnimatedSprite.play("dead")
+		$CollisionShape2D.disabled=true
+		$Timer.start()
+		if scale >Vector2(1,1):
+			get_parent().get_node("ScreenShake").screen_shake(1,10,100)
+			
 	
 func _physics_process(delta):
 	if is_dead==false:
-		velocity.x=SPEED*direction 
+		velocity.x=speed*direction 
 		$AnimatedSprite.play("walk")
 		if direction==1:
 			$AnimatedSprite.flip_h=false
@@ -30,9 +39,11 @@ func _physics_process(delta):
 		velocity =move_and_slide(velocity ,FLOOR)
 	if is_on_wall():
 		direction*=-1
+	if get_slide_count()>0:
+		for i in range(get_slide_count()):
+			if "Player" in get_slide_collision(i).collider.name:
+				get_slide_collision(i).collider.dead()	
 		
-	
-	
 		
 
 func _on_Timer_timeout():
